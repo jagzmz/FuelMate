a5fbe106800305c12a746e5e94412e9b9e508f07
Mahesh <jagzmz@gmail.com>
2019-03-13 00:15:48 +0530

    Foreground Effect on cardview...

--- app/src/main/java/com/example/fuelmate/search.java
+++ app/src/main/java/com/example/fuelmate/search.java
@@ -1,6 +1,7 @@
 package com.example.fuelmate;
 
 import android.app.usage.NetworkStats;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -30,7 +31,7 @@ public class search extends Fragment {
 
     private RecyclerView mUsersList;
     private DatabaseReference mDbRef;
-    public static String pref,colg;
+    public static String pref, colg;
 
 
     @Nullable
@@ -43,26 +44,23 @@ public class search extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mUsersList=(RecyclerView)getView().findViewById(R.id.user_view);
+        mUsersList = (RecyclerView) getView().findViewById(R.id.user_view);
         mUsersList.setHasFixedSize(true);
         mUsersList.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
-        mDbRef= FirebaseDatabase.getInstance().getReference();
+        mDbRef = FirebaseDatabase.getInstance().getReference();
 
 
         final FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        mDbRef= FirebaseDatabase.getInstance().getReference().child("Users").child(mUser.getUid());
+        mDbRef = FirebaseDatabase.getInstance().getReference().child("Users").child(mUser.getUid());
 
-        pref=((TextView)getActivity().findViewById(R.id.nav_pref)).getText().toString();
-        colg=((TextView)getActivity().findViewById(R.id.nav_college)).getText().toString();
-
-
-
-        Query qry=FirebaseDatabase.getInstance().getReference().child("Preferences/"+pref+"/"+colg);
+        pref = ((TextView) getActivity().findViewById(R.id.nav_pref)).getText().toString();
+        colg = ((TextView) getActivity().findViewById(R.id.nav_college)).getText().toString();
 
 
+        Query qry = FirebaseDatabase.getInstance().getReference().child("Preferences/" + pref + "/" + colg);
 
 
         FirebaseRecyclerOptions<users> options =
@@ -71,91 +69,96 @@ public class search extends Fragment {
                             @NonNull
                             @Override
                             public users parseSnapshot(@NonNull DataSnapshot snapshot) {
-                                if(!snapshot.getKey().equals(mUser.getUid())) {
+                                if (!snapshot.getKey().equals(mUser.getUid())) {
                                     return new users(snapshot.child("name").getValue().toString(),
                                             snapshot.child("college").getValue().toString());
-                                }
-                                else {
+                                } else {
                                     return new users();
                                 }
                             }
                         })
                         .build();
 
-         FirebaseRecyclerAdapter<users,UserViewHolder> firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<users, UserViewHolder>(options) {
+        FirebaseRecyclerAdapter<users, UserViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<users, UserViewHolder>(options) {
 
-             @Override
-             protected void onBindViewHolder(@NonNull UserViewHolder holder, final int position, @NonNull users model) {
-
-
-
-                     if(model.getName()==null)
-                     {
-                         holder.itemView.setVisibility(View.GONE);
-                         holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
-                     }
-                     else {
-                         holder.setname(model.getName());
-                         holder.setColg(model.getColg());
-
-                     }
-
-                 holder.itemView.setOnClickListener(new View.OnClickListener() {
-                     @Override
-                     public void onClick(View v) {
-                         View v1 = v.findViewById(R.id.accept);
-                         View v2 = v.findViewById(R.id.information);
-
-                         if (v1.getVisibility() == View.GONE) {
-                             v1.setVisibility(View.VISIBLE);
-                             v2.setVisibility(View.VISIBLE);
-                         } else {
-                             v1.setVisibility(View.GONE);
-                             v2.setVisibility(View.GONE);
-                         }
+            @Override
+            protected void onBindViewHolder(@NonNull UserViewHolder holder, final int position, @NonNull users model) {
 
 
-                     }
-                 });
+                if (model.getName() == null) {
+                    holder.itemView.setVisibility(View.GONE);
+                    holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
+                } else {
+                    holder.setname(model.getName());
+                    holder.setColg(model.getColg());
+
+                }
+
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.findViewById(R.id.accept).setVisibility(View.GONE);
+                    }
+                });
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        View v1 = v.findViewById(R.id.accept);
+                        View v2 = v.findViewById(R.id.information);
+
+                        if (v1.getVisibility() == View.GONE) {
+
+                            v1.setVisibility(View.VISIBLE);
+                            v2.setVisibility(View.VISIBLE);
+                        } else {
+                            v1.setVisibility(View.GONE);
+                            v2.setVisibility(View.GONE);
+                        }
 
 
-             }
-
-             @NonNull
-             @Override
-             public UserViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+                    }
+                });
 
 
-                 View view = LayoutInflater.from(parent.getContext())
-                         .inflate(R.layout.single_user_view, parent, false);
+            }
 
-                 return new UserViewHolder(view);
-             }
-         };
+            @NonNull
+            @Override
+            public UserViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+
+
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.single_user_view, parent, false);
+
+                return new UserViewHolder(view);
+            }
+        };
 
         mUsersList.setAdapter(firebaseRecyclerAdapter);
+
+
         firebaseRecyclerAdapter.startListening();
     }
 
-    public class UserViewHolder extends RecyclerView.ViewHolder{
+    public class UserViewHolder extends RecyclerView.ViewHolder {
 
         LinearLayout root;
 
-        public UserViewHolder(View itemV)
-        {
+        public UserViewHolder(View itemV) {
             super(itemV);
-            root=itemView.findViewById(R.id.userSingleLayout);
+            root = itemView.findViewById(R.id.userSingleLayout);
         }
 
         public void setname(String name) {
 
-            TextView dname=root.findViewById(R.id.name);
+            TextView dname = root.findViewById(R.id.name);
             dname.setText(name);
         }
 
         public void setColg(String name) {
 
-            TextView cname=root.findViewById(R.id.college);
+            TextView cname = root.findViewById(R.id.college);
             cname.setText(name);
         }
     }
